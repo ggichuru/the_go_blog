625d4a5055bb7319f7c6f6bc0f4652dc408c016a
ggichuru <ggichuru.dev@gmail.com>
2023-03-24 22:45:55 +0300

    [ config ] -> Db configuration

---
+++ initializers/connectDB.go
@@ -0,0 +1,25 @@
+package initializers
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Store the conncetion object in the gorm.DB struct
+var DB *gorm.DB
+
+// Creates a new connection pool with the Postgress DB
+func ConnectDB(config *Config) {
+	var err error
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbmane=%s port=%s sslmode=disable TimeZone=Africa/Nairobi", config.DBHost, config.DBUserName, config.DBUserPassword, config.DBName, config.DBPort)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to the Database")
+	}
+	fmt.Println("? Connected Successfully to the Database")
+}
