3497a87010d1c960814826c6406e2dccaaccf2e7
ggichuru <ggichuru.dev@gmail.com>
2023-03-25 21:30:16 +0300

    [ controller ] - sign up users

--- routes/auth.routes.go
+++ routes/auth.routes.go
@@ -1,6 +1,9 @@
 package routes
 
-import "github.com/ggichuru/the_go_blog/controllers"
+import (
+	"github.com/ggichuru/the_go_blog/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 type AuthRouteController struct {
 	authController controllers.AuthController
@@ -8,4 +11,10 @@ type AuthRouteController struct {
 
 func NewAuthRouteController(authController controllers.AuthController) AuthRouteController {
 	return AuthRouteController{authController}
+}
+
+func (arc *AuthRouteController) AuthRoute(rg *gin.RouterGroup) {
+	router := rg.Group("auth")
+
+	router.POST("/register", arc.authController.)
 }
