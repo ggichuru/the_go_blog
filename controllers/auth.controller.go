940340f67f69688199fed5f2fa1f7130a6daddff
ggichuru <ggichuru.dev@gmail.com>
2023-03-25 21:30:54 +0300

    [controller ] sign up user

--- controllers/auth.controller.go
+++ controllers/auth.controller.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "gorm.io/gorm"
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/ggichuru/the_go_blog/models"
+	"github.com/ggichuru/the_go_blog/utils"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type AuthController struct {
 	DB *gorm.DB
@@ -8,4 +17,60 @@ type AuthController struct {
 
 func NewAuthController(DB *gorm.DB) AuthController {
 	return AuthController{DB}
+}
+
+func (ac *AuthController) SignUpUser(ctx *gin.Context) {
+	var payload *models.SignUpInput
+
+	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	if payload.Password != payload.PasswordConfirm {
+		ctx.JSON(http.StatusBadRequest, gin.H{"status": "fail", "message": "Passwords do not match"})
+		return
+	}
+
+	hashedPwd, err := utils.HashPassword(payload.Password)
+	if err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	now := time.Now()
+	newUser := models.User{
+		Name:      payload.Name,
+		Email:     payload.Email,
+		Password:  hashedPwd,
+		Role:      "user",
+		Verified:  true,
+		Photo:     payload.Photo,
+		Provider:  "local",
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	result := ac.DB.Create(&newUser)
+
+	if result.Error != nil && strings.Contains(result.Error.Error(), "duplicate key value violates unique") {
+		ctx.JSON(http.StatusConflict, gin.H{"status": "fail", "message": "User with that email already exists"})
+		return
+	} else if result.Error != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "error", "message": "something bad happened"})
+		return
+	}
+
+	userResponse := &models.UserResponse{
+		ID:        newUser.ID,
+		Name:      newUser.Name,
+		Email:     newUser.Email,
+		Role:      newUser.Role,
+		Photo:     newUser.Photo,
+		Provider:  newUser.Provider,
+		CreatedAt: newUser.CreatedAt,
+		UpdatedAt: newUser.UpdatedAt,
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"status": "success", "data": gin.H{"user": userResponse}})
 }
