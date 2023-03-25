7a4a5d64d8af0a20de45b9a1a16002089f31bc5c
ggichuru <ggichuru.dev@gmail.com>
2023-03-25 20:06:19 +0300

    [ routes ] - post routes

---
+++ routes/post.routes.go
@@ -0,0 +1,27 @@
+package routes
+
+import (
+	"github.com/ggichuru/the_go_blog/controllers"
+	"github.com/ggichuru/the_go_blog/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+type PostRouteController struct {
+	postController controllers.PostController
+}
+
+func NewRoutePostController(postController controllers.PostController) PostRouteController {
+	return PostRouteController{postController}
+}
+
+func (prc *PostRouteController) PostRoute(rg *gin.RouterGroup) {
+	router := rg.Group("posts")
+
+	router.Use(middleware.DeserializeUser())
+
+	router.POST("/", prc.postController.CreatePost)
+	router.GET("/", prc.postController.FindPosts)
+	router.PUT("/:postId", prc.postController.UpdatePost)
+	router.GET("/:postId", prc.postController.FindPostById)
+	router.DELETE("/:postId", prc.postController.DeletePost)
+}
