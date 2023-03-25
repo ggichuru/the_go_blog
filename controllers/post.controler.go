fd932bb204bed49ca34742899d66e0ab9213dfd4
ggichuru <ggichuru.dev@gmail.com>
2023-03-25 18:10:57 +0300

    [ controllers ] -> update post

--- controllers/post.controler.go
+++ controllers/post.controler.go
@@ -55,3 +55,42 @@ func (pc *PostController) CreatePost(ctx *gin.Context) {
 	// Return a JSON response with the newly created post to the client
 	ctx.JSON(http.StatusCreated, gin.H{"status": "success", "data": newPost})
 }
+
+// [...] Update post handler
+func (pc *PostController) UpdatePost(ctx *gin.Context) {
+	// Extract post ID from the request parameters
+	postId := ctx.Param("postId")
+	currentUser := ctx.MustGet("currentUser").(models.User)
+
+	var payload *models.UpdatePost
+	if err := ctx.ShouldBind(&payload); err != nil {
+		ctx.JSON(http.StatusBadGateway, gin.H{"status": "fail", "message": err.Error()})
+		return
+	}
+
+	var updatedPost models.Post
+
+	// Check if the post ID exists
+	result := pc.DB.First(&updatedPost, "id = ?", postId)
+	if result.Error != nil {
+		ctx.JSON(http.StatusNotFound, gin.H{"status": "fail", "message": "No post with that title exists"})
+		return
+	}
+
+	now := time.Now()
+
+	// Construct a post argument
+	postToUpdate := models.Post{
+		Title:     payload.Title,
+		Content:   payload.Content,
+		Image:     payload.Image,
+		User:      currentUser.ID,
+		CreatedAt: updatedPost.CreatedAt,
+		UpdatedAt: now,
+	}
+
+	// Update post
+	pc.DB.Model(&updatedPost).Updates(postToUpdate)
+
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "data": updatedPost})
+}
